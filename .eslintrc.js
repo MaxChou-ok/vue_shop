d3cdd1425418b60804ef78d64024ef9d512bf632
MaxChou-ok <569143518@qq.com>
2020-03-12 09:34:50 +0800

    dengluyemian

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/essential',
@@ -11,7 +11,35 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+    'no-console': 0,
+    'no-debugger': 0,
+    'no-empty':0,
+    'space-before-function-paren': 0, //函数参数前面要加空格
+    'space-before-blocks': 0, //函数块前面要加空格
+    'semi': 0 ,//分号检查
+    'no-useless-return':0,
+    'camelcase': 0,
+        'indent': 0,
+        'no-tabs': 0,
+        'no-mixed-spaces-and-tabs': 0,
+        'object-curly-spacing': 0,
+        'space-before-function-paren': 0,
+        'no-trailing-spaces': 0,
+        'vue/multiline-html-element-content-newline': 0,
+        'vue/html-closing-bracket-newline': 0,
+        'vue/html-indent': ['error', 4, {
+            'attribute': 1,
+            'baseIndent': 1,
+            'closeBracket': 0,
+            'alignAttributesVertically': true,
+            'ignores': []
+        }],
+        'vue/max-attributes-per-line': ['error', {
+            'singleline': 1,
+            'multiline': {
+                'max': 1,
+                'allowFirstLine': true
+            }
+        }]
+  },
 }
