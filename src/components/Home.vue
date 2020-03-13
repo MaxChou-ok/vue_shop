b89edd0df794b9059f4dce714bd8eaee4af6713c
MaxChou-ok <569143518@qq.com>
2020-03-13 10:06:22 +0800

    用户列表

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,147 @@
 <template>
-    <div>
-        <el-button type="info" 
-                   @click="logout">退出</el-button>
-    </div>
+    <el-container class="home-container">
+        <!-- 头部 -->
+        <el-header>
+            <div>
+                <img src="../assets/heima.png" 
+                     alt="">
+                <span>电商后台管理系统</span>
+            </div>
+            <el-button type="info" 
+                       @click="logout">退出</el-button>
+        </el-header>
+        <!-- 页面主题 -->
+        <el-container>
+            <!-- 侧边栏 -->
+            <el-aside :width="isCollapse ? '64px' : '200px'">
+                <!-- 展开缩回 -->
+                <div class="toggle-button"
+                     @click="toggleCollapse">111</div>
+                <!-- 侧边栏菜单区域 -->
+                <el-menu
+                    background-color="#545c64"
+                    text-color="#fff"
+                    active-text-color="#ffd04b"
+                    unique-opened
+                    :collapse="isCollapse"
+                    :collapse-transition="false"
+                    router
+                    :default-active="activePath"
+                >
+                    <!-- 一级菜单 -->
+                    <el-submenu :index="item.id + ''"
+                                v-for="item in menulist"
+                                :key="item.id">
+                        <template slot="title">
+                            <i :class="iconsObj[item.id]"></i>
+                            <span>{{item.authName}}</span>
+                        </template>
+                        <!-- 二级菜单 -->
+                        <el-menu-item :index="'/'+subItem.path"
+                                      v-for="subItem in item.children"
+                                      :key="subItem.id"
+                                      @click="saveNavState('/'+subItem.path)"><template slot="title">
+                                          <i class="el-icon-menu"></i>
+                                          <span>{{subItem.authName}}</span>
+                                      </template></el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
+            <!-- 右侧内容主体 -->
+            <el-main>
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
 export default {
+    data(){
+        return {
+            // 左侧菜单数据
+            menulist: [],
+            iconsObj: {
+                125: 'iconfont icon-user',
+                103: 'iconfont icon-tijikongjian',
+                101: 'iconfont icon-shangpin',
+                102: 'iconfont icon-danju',
+                145: 'iconfont icon-baobiao'
+            },
+            // 是否折叠
+            isCollapse: false,
+            // 被激活的链接地址
+            activePath: ''
+        }
+    },
+    created(){
+        this.getMenuList()
+        this.activePath = window.sessionStorage.getItem('activePath')
+    },
   methods: {
     logout () {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    // 获取所有的菜单
+    async getMenuList(){
+        const {data: res} = await this.$http.get('menus')
+        if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+        this.menulist = res.data
+        console.log(res)
+    },
+    // 点击按钮折叠菜单
+    toggleCollapse() {
+        this.isCollapse = !this.isCollapse
+    },
+    // 保存连接的激活状态
+    saveNavState(activePath) {
+        window.sessionStorage.setItem('activePath', activePath)
+        this.activePath = activePath
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.home-container {
+    height: 100%;
+}
+.el-header {
+    background-color:#373d41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: white;
+    font-size: 20px;
+    > div {
+        display: flex;
+        align-items: center;
+        span {
+            margin-left: 15px;
+        }
+    }
+}
+.el-aside {
+    background-color:#333744;
+    .el-menu {
+        border-right: none;
+    }
+}
+.el-main {
+    background-color: #eaedf1;
+}
+.iconfont {
+    margin-right: 10px;
+}
+.toggle-button {
+    background-color: #4a5064;
+    font-size: 10px;
+    line-height: 24px;
+    color: white;
+    text-align: center;
+    letter-spacing: 0.2em;
+    cursor: pointer;
+}
 </style>
